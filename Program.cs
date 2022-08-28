4686111ed6eec8980f49169517662fd93551aef0
Alik <9774952689@bk.ru>
2022-08-28 22:17:47 +0300

    Сделал задание №1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine("Максимальное число " + a);
+    Console.WriteLine("Минимальное число " + b);
+}    
+if(b > a)
+{    
+    Console.WriteLine("Максимальное число " +a);
+    Console.WriteLine("Минимальное число " + b);
+}
+if(b == a)
+    Console.Write("Вы ввели одинмаковые числа, попробуйте ещё раз) ");
